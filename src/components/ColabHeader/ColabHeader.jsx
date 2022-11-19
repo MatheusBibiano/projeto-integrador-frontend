449bd3e421399ac833c6e9b9a8097e4c6798ce56
Matheus Bibiano Alves <matheusbibiano30@gmail.com>
2022-11-19 14:20:58 -0300

    fix: solve responsive profile

--- src/components/ColabHeader/ColabHeader.jsx
+++ src/components/ColabHeader/ColabHeader.jsx
@@ -6,19 +6,24 @@ export function ColabHeader({ discName, personName, hasBackButton }) {
       className={`
           fixed top-0 z-50
           flex justify-between items-center
-          py-4 px-10
+          py-4 px-4 md:px-10
           bg-[#201f22]/80
           border-b-2 border-[#151416]/30
           w-full
           backdrop-blur-md
-          ${hasBackButton && "pl-24"}
+          ${hasBackButton && "md:pl-24"}
         `}
     >
-      <h1 className="text-3xl md:text-5xl text-[#FEFEFE] max-w-fit">
+      <h1 className="text-2xl md:text-5xl text-[#FEFEFE] max-w-fit">
         {discName}
       </h1>
       <div className="flex items-center gap-2">
-        <span className="text-[#FEFEFE]/80 text-lg">{personName}</span>
+        <span className="hidden text-[#FEFEFE]/80 text-lg md:block">
+          {personName}
+        </span>
+        <span className="block text-[#FEFEFE]/80 text-lg md:hidden">
+          {personName.split(" ")[0]}
+        </span>
         <div className="flex items-center justify-center p-2 bg-[#FEFEFE]/80 rounded-full">
           <BsPerson size={24} />
         </div>
